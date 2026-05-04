bc3d75fb6ddd2a72430315fd037c1494b67593d8
juncie <junciestudios@gmail.com>
2026-05-03 19:05:18 -0700

    [FEAT]: Turborepo setup.

--- apps/extension/entrypoints/popup/App.tsx
+++ apps/extension/entrypoints/popup/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
-import './App.css';
+import { useState } from "react";
+import reactLogo from "@/assets/react.svg";
+import wxtLogo from "/wxt.svg";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,15 +10,25 @@ function App() {
     <>
       <div>
         <a href="https://wxt.dev" target="_blank">
-          <img src={wxtLogo} className="logo" alt="WXT logo" />
+          <img
+            src={wxtLogo}
+            className="logo"
+            alt="WXT logo"
+          />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img
+            src={reactLogo}
+            className="logo react"
+            alt="React logo"
+          />
         </a>
       </div>
       <h1>WXT + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </button>
         <p>
